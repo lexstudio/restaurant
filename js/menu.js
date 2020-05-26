8ccae283f6b6c4b44ab4c17172d0ffd4acecd573
Alexander Moreno <alexander.morenoc@outlook.com>
2020-05-25 20:26:39 -0500

    Banner y boton de menu funcionando

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let btnMenu = document.getElementById("btnmenu")
+let menu = document.getElementById("menu")
+
+btnMenu.addEventListener("click", () => {
+  "use strict"
+  menu.classList.toggle(`mostrar`)
+})
